9f0610807660da65bf4d7dc320cc3be8ecfcbaac
ridhii <rsurana@andrew.cmu.edu>
2017-09-23 15:23:03 -0400

    Node and Node_part1.sv header matches the one in handout

--- Node_part1.sv
+++ Node_part1.sv
@@ -6,27 +6,27 @@
 ////// Network on Chip (NoC) 18-341
 ////// Node module
 //////
-module Node #(parameter NODEID=0) (
-    input  logic       clock, reset_n,
+module Node #(parameter NODEID = 0) (
+  input logic clock, reset_n,
 
-    input pkt_t        pkt_in,            // Data packet from TB
-    input logic        pkt_in_avail,      // The packet from TB is available
-    output logic       cQ_full,           // The queue is full
+  //Interface to testbench: the blue arrows
+  input  pkt_t pkt_in,        // Data packet from the TB
+  input  logic pkt_in_avail,  // The packet from TB is available
+  output logic cQ_full,       // The queue is full
 
-    output pkt_t       pkt_out,           // Outbound packet from node to TB
-    output logic       pkt_out_avail,     // The oubound packet is available
+  output pkt_t pkt_out,       // Outbound packet from node to TB
+  output logic pkt_out_avail, // The outbound packet is available
 
-    input logic        free_outbound,     // Node is free
-    input logic        put_inbound,       // Node is transferring to router
-    input logic [7:0]  payload_inbound,   // Data sent from node to router
+  //Interface with the router: black arrows
+  input  logic       free_outbound,    // Router is free
+  output logic       put_outbound,     // Node is transferring to router
+  output logic [7:0] payload_outbound, // Data sent from node to router
 
-    output logic       free_inbound,      // Router is free
-    output logic       put_outbound,      // Router is transferring to node
-    output logic [7:0] payload_outbound); // Data sent from node to router
-
+  output logic       free_inbound,     // Node is free
+  input  logic       put_inbound,      // Router is transferring to node
+  input  logic [7:0] payload_inbound); // Data sent from router to node
 
 endmodule : Node
-
 /*
  *  Create a FIFO (First In First Out) buffer with depth 4 using the given
  *  interface and constraints
